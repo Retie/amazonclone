d1b1b6fb083134217e1b39eb811de0b767b849bb
Retie <csy0797@gmail.com>
2021-12-29 16:19:54 +0900

    MemberAccount.java

---
+++ amazon/src/main/java/clone/amazon/domain/MemberAccount.java
@@ -0,0 +1,12 @@
+package clone.amazon.domain;
+
+public class MemberAccount {
+	//PK
+	public Long id;
+
+	public String name;
+	public String email;
+	public String phoneNum;
+	public String pwd;
+	public String otp;
+}
