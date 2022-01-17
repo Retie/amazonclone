a0fb9161d7b27af87129f0e616796943a0c9ef62
수영 <csy0797@gmail.com>
2022-01-18 00:30:06 +0900

    memberJoinCheck.js ajax 부분 수정하기

--- src/main/webapp/resources/js/memberJoinCheck.js
+++ src/main/webapp/resources/js/memberJoinCheck.js
@@ -63,8 +63,28 @@ function joinCheck(){
 			if(responseData=="success"){
 				window.location.href = "/"
 			}
-		},
+			
+			
+			/* 여기를 긁어왔으니 수정해야 합니다 */
+			if(res = 1) {
+				$that.attr('state', 'true');
+				$that.css('border', '1px solid green');
+				$('.email-alert').css('color','green').html('사용할 수 있는 이메일입니다.').stop().fadeIn();
+				$that.attr('data-state', 'true');
+			}else{
+				$that.attr('state', 'false');
+				$that.css('border', '1px solid red');
+				$('.email-alert').css('color','red').html('이미 사용중이거나 중복된 이메일 입니다.').stop().fadeIn();
+			}
+		}, error :  function(request,status,error){// 에러발생시 실행할 함수
+			alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+			}
+			
+			
+			
+/*		},
 		error: function(){},
+	*/	
 		
 	})
 	activeFunc();
@@ -75,3 +95,31 @@ function activeFunc(){
 	console.log("실행");
 	
 }
+
+$('#temp_input').bind('blur', function(){
+	var email = $(this).val(), $that = $(this);
+	
+	console.log('email :: '+ email);
+	
+	
+	$.ajax({
+		url : '<?=$DC[skinurl]?>/api_check_email.php',
+		type : 'POST',
+		data : {email : email},
+		success : function(res) {
+			console.log('성공: '+res);
+			if(res = 1) {
+				$that.attr('state', 'true');
+				$that.css('border', '1px solid green');
+				$('.email-alert').css('color','green').html('사용할 수 있는 이메일입니다.').stop().fadeIn();
+				$that.attr('data-state', 'true');
+			}else{
+				$that.attr('state', 'false');
+				$that.css('border', '1px solid red');
+				$('.email-alert').css('color','red').html('이미 사용중이거나 중복된 이메일 입니다.').stop().fadeIn();
+			}
+		}, error :  function(request,status,error){// 에러발생시 실행할 함수
+			alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+			}
+	});
+});
